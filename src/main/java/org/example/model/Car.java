420121b56f98a671e1a8c9773902fa4bcfce1108
Tomasz9922 <tomas9922@o2.pl>
2022-11-22 17:24:59 +0100

    Added Car and Employee classes

---
+++ src/main/java/org/example/model/Car.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+public class Car {
+    String producer;
+    String model;
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "producer='" + producer + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
